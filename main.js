075a06ce7490c6a5999560a37da6156c64f14a00
suusara <smataeva09@gmail.com>
2023-07-26 15:58:45 +0600

    add styles

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const mainbox = document.querySelector('.mainbox');
+const inputs = document.querySelectorAll('.custom-input');
 
+const update = () => inputs.forEach((input) => input.checked = mainbox.checked);
+
+mainbox.addEventListener('input',update,false);
